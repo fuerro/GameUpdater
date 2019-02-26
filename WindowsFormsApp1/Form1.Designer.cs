bbdf5d9255155bf308aca694dc8786f3a9dabc58
Robin Füglister <robin.fueglister@qlgroup.ch>
2019-02-26 23:33:22 +0100

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class form_gamesupdater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.dataGridView_links = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_links)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_exit
+            // 
+            this.button_exit.Location = new System.Drawing.Point(656, 397);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(132, 41);
+            this.button_exit.TabIndex = 0;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(518, 397);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(132, 40);
+            this.button_refresh.TabIndex = 1;
+            this.button_refresh.Text = "Refresh";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // dataGridView_links
+            // 
+            this.dataGridView_links.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_links.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView_links.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView_links.Name = "dataGridView_links";
+            this.dataGridView_links.Size = new System.Drawing.Size(775, 378);
+            this.dataGridView_links.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Online Version";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Local Version";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Link";
+            this.Column4.Name = "Column4";
+            // 
+            // form_gamesupdater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_links);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.button_exit);
+            this.Name = "form_gamesupdater";
+            this.Text = "GamesUpdater";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_links)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.DataGridView dataGridView_links;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class form_gamesupdater : Form
+    {
+        public form_gamesupdater()
+        {
+            InitializeComponent();
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            using (System.IO.TextReader tr = new StreamReader("C:\\tmp\\Links_text.txt"))
+            {
+                string line;
+                while ((line = tr.ReadLine()) != null)
+                {
+
+                    string[] items = line.Trim().Split(',');
+                    if (dt.Columns.Count == 0)
+                    {
+                        // Create the data columns for the data table based on the number of items
+                        // on the first line of the file
+                        for (int i = 0; i < items.Length; i++)
+                            dt.Columns.Add(new DataColumn("Column" + i, typeof(string)));
+                    }
+                    dt.Rows.Add(items);
+
+                }
+                //show it in gridview
+                this.dataGridView_links.DataSource = dt;
+
+                //if ((dataGridView_links.Columns[1].ToString()).Equals(dataGridView_links.Columns[2].ToString()))
+                //    dataGridView_links.Columns[4]. = "Yes";
+            }
+        }
+    }
+}
